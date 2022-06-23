a2b81fa5d4d71ff33c791b13cb897155bd44b8ea
Onur Uysal <onur.uysal@ug.bilkent.edu.tr>
2022-06-23 18:19:47 +0300

    No idea

--- models/DatabaseConnection.java
+++ models/DatabaseConnection.java
@@ -2,23 +2,44 @@ package models;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
     Connection connection;
+    Statement statement;
+    ResultSet resultSet;
 
     public DatabaseConnection() {
-        // try {
-        // Class.forName("com.mysql.jdbc.Driver");
-        // } catch (ClassNotFoundException e1) {
-        // e1.printStackTrace();
-        // }
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BattleShips", "root", "onur2001");
             System.out.println("Connected to database");
         } catch (SQLException e) {
-            e.printStackTrace();
             System.out.println("Database connection failed");
         }
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            // TODO
+            e.printStackTrace();
+        }
+    }
+
+    public boolean createUser(String userName, String password, String mail) {
+        try {
+            statement.executeUpdate(
+                    "INSERT INTO users VALUES ( 0, '" + userName + "', '" + password + "', 0, NULL,'" + mail + "');");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Database failed adding account");
+            return false;
+        }
+    }
+
+    private static int generateID() {
+        // TODO
+        return 1;
     }
 }
--- models/Server.java
+++ models/Server.java
@@ -1,14 +1,35 @@
 package models;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server implements Runnable {
     ServerSocket ss;
     Socket s;
     DatabaseConnection database;
+    BufferedReader in;
+    PrintWriter out;
 
     public Server() {
         database = new DatabaseConnection();
+        database.createUser("eu", "ppp", "maill");
+    }
+
+    class Connection {
+        String IP;
+        BufferedReader in;
+        PrintWriter out;
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void start() {
+
     }
 }
